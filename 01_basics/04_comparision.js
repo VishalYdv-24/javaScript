0074277e5d538b451c3d2e34c703de2c88572ce6
Vishal <vishal2004500@gmail.com>
2025-06-30 21:11:26 +0530

    04_comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+// ytLink : https://www.youtube.com/watch?v=giP2uXMlv4c&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=8
+
+// SIMPLE comparision
+/*
+2 > 1 => true
+2 >= 1 => true
+2 < 1 => false
+2 == 1 => false
+2 != 1 => true
+ */
+
+// comparing different types 
+// console.log("1" > 2); F
+// console.log(1 < "2"); T
+
+// null
+// console.log(null > 0); F
+// console.log(null == 0); F
+// console.log(null >= 0); T
+
+// for undifined it always False
+
+// === also check dataTypes while comparing
+// "2" === 2 => false
+// 2===2 => true
+
