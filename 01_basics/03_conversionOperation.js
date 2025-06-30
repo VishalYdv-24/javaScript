d19a10b8d4d95ac61f1f289abd40f515106a4786
Vishal <vishal2004500@gmail.com>
2025-06-30 17:35:19 +0530

    03_Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,35 +1,43 @@
-let score = 24;
-let points = "17abc";
-console.log(typeof(points));
+// let score = 24;
+// let points = "17abc";
+// console.log(typeof(points));
 
-console.log(typeof score);
+// console.log(typeof score);
 
-// converting points (string) into number
-let valueInNumber = Number(points);
-console.log(valueInNumber);
+// // converting points (string) into number
+// let valueInNumber = Number(points);
+// console.log(valueInNumber);
 
-console.log(typeof(valueInNumber));
+// console.log(typeof(valueInNumber));
 
-// converting into Numbers then: 
-// "33" => 33
-// "33abc" => NaN
-// true => 1
-// null => 0
-// undefined => NaN
+// // converting into Numbers then: 
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1
+// // null => 0
+// // undefined => NaN
 
-let loggedIn = 1;
-let boolLoggedIn = Boolean(loggedIn);
-console.log(typeof boolLoggedIn);
-console.log(boolLoggedIn)
+// let loggedIn = 1;
+// let boolLoggedIn = Boolean(loggedIn);
+// console.log(typeof boolLoggedIn);
+// console.log(boolLoggedIn)
 
-// converting into booleans
-// 12 => true
-// 1 => true
-// 0 => false
-// "" => false
-// "vishal" => true
-// -45 => true
+// // converting into booleans
+// // 12 => true
+// // 1 => true
+// // 0 => false
+// // "" => false
+// // "vishal" => true
+// // -45 => true
 
 
-// converting into string
-// 1 => string
+// // converting into string
+// // 1 => string
+
+
+
+// ----------Operations---------
+let value = 5;
+let negValue = -value;
+console.log(negValue);
+console.log(typeof negValue);
