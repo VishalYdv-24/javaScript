5e7328d6a823ff1b133961ae2c387c318b0df1db
Vishal <vishal2004500@gmail.com>
2025-06-30 21:00:19 +0530

    03_operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+// ytLink : https://youtu.be/X7hDBhd_L5U?si=FDPgolX0fuByzrB0
+
 // let score = 24;
 // let points = "17abc";
 // console.log(typeof(points));
@@ -37,7 +39,28 @@
 
 
 // ----------Operations---------
+// ytLink : https://www.youtube.com/watch?v=N9el4APFtAo&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=8
 let value = 5;
 let negValue = -value;
 console.log(negValue);
 console.log(typeof negValue);
+
+//operations
+// +
+// -
+// *
+// **
+// /
+// %
+//  "vis"+"hal" => "vishal"
+
+
+// concatenation
+// "1" + 2 => 12
+//  1 + "2" => 12
+//  "1" + 2 + 2 => 122
+//  1 + 2 + "2" => 32  for this follow ecma standard guidelines for addition of different data types
+// link : https://tc39.es/ecma262/multipage/abstract-operations.html
+
+// console.log(+true) => 1
+// +"" => 0
