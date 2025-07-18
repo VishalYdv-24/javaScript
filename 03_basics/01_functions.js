552097cdc870e7d00d4da7f65b27a0421f3ac020
Vishal <vishal2004500@gmail.com>
2025-07-18 21:07:23 +0530

    01_function_part01

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+// 01_function_part01
+// ytLink : https://www.youtube.com/watch?v=Bn56WahG_t0&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=19
+
+// in this part he introduced with basic function parameters, like return, syntax, and how to call, which i knew already so i did;nt wroks on this into code.
+
+console.log("01_fucnion_part01");
+
+const sum = function(num1,num2){
+    return num1+num2;
+   
+} 
+
+num1 =5;
+num2 =6;
+
+
+console.log(sum(num1,num2));
+
+
+
