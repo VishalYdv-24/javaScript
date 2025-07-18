8fe58f144917b4946cafa84e7683564d847ebec8
Vishal <vishal2004500@gmail.com>
2025-07-18 21:23:11 +0530

    01_function_part02

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -14,7 +14,35 @@ num1 =5;
 num2 =6;
 
 
-console.log(sum(num1,num2));
+// console.log(sum(num1,num2));
+
+console.log(" ");
 
 
+// 01_fucnion_part02
+// ytLink : https://www.youtube.com/watch?v=t7ZHPhgdA4U&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=20
+console.log("01_fucnion_part02");
 
+const calculateCartPrice = function(...num){
+    return num;
+}
+
+// console.log(calculateCartPrice(500,100,150,800));
+
+
+const miniProject = {
+    totalMember : 3,
+    title : "Smart Vaccum Cleaner",
+    memberNames : ["Vishal","Shreyash","Srushti"],
+    memberRollNo : [24,25,26],
+    teamLeader : "Vishal"
+}
+
+function handleObject(anyObject) {
+    console.log(`Project Title: ${anyObject.title}`);
+    console.log(`Team Leader: ${anyObject.teamLeader}`);
+    console.log(`Team Members: ${anyObject.memberNames.join(", ")}`);
+    
+}
+
+handleObject(miniProject);
