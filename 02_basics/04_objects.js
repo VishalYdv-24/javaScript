80eb6af60c275e7d618e28f9dc33cc7bc8b02546
Vishal <vishal2004500@gmail.com>
2025-07-18 20:38:47 +0530

    04_Object_part02

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,4 +1,8 @@
+// 04_Objects_Part01
 // ytLink : https://www.youtube.com/watch?v=4lb2pXWWXJI&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=17
+
+
+console.log("04_Object_Part01");
 
 // singleton object 
 // const pubg = new Object();
@@ -68,10 +72,31 @@ const DBdata = [
 
 // will use pubg object as example to fetch keys/values/entries(paair of both key : value) of any object and that would be into array 
 
-console.log(Object.keys(pubg));
-console.log(Object.values(pubg));
-console.log(Object.entries(pubg));
+// console.log(Object.keys(pubg));
+// console.log(Object.values(pubg));
+// console.log(Object.entries(pubg));
 
 // to check if the object has that key or not 
-console.log(pubg.hasOwnProperty('name'));
+// console.log(pubg.hasOwnProperty('name'));
 
+
+
+// 04_Objects_Part02
+// ytLink : https://www.youtube.com/watch?v=AViTh83k-IE&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=18
+console.log("04_Object_Part02   ");
+
+
+const miniProject = {
+    totalMember : 3,
+    title : "Smart Vaccum Cleaner",
+    memberNames : ["Vishal","Shreyash","Srushti"],
+    memberRollNo : [24,25,26],
+    teamLeader : "Vishal"
+}
+// destructuring
+const {teamLeader : leader} = miniProject;
+// console.log(miniProject.teamLeader);
+// console.log(leader);
+
+// basic API
+// ytLink : https://youtu.be/AViTh83k-IE?si=H7E4620kXh7qA-mf&t=435  go through this link 
