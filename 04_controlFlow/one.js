325dd9fbe3152133ff68fdceaa77e13cd178fa2d
Vishal <vishal2004500@gmail.com>
2025-07-19 22:09:05 +0530

    ControlFlow one.js

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,27 @@
+// ytLink : https://www.youtube.com/watch?v=0P_YvC6Gg0c&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=26
+
+
+
+// in this he has explained how if else works 
+// switch case method 
+/*
+switch (key) {
+    case value :
+        break;
+    
+        default :
+        break;
+}
+*/
+
+
+// falsy value : means value which is false by default if nothing in initiated into it;
+// falsy value list : false, 0, -0,BigInt 0n, "" ,null, undefined ,NaN
+
+// turthy value : "0","false"," ",[],{},function(){}
+
+// nullish coalescing Operator (??) null and undefined are used mostly here 
+
+let val1 = null ?? 5;
+console.log(val1);
+
