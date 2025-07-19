587be85d70ad1858e207154ad5f61e0e245ed474
Vishal <vishal2004500@gmail.com>
2025-07-19 11:57:44 +0530

    02_scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,8 @@
+// ytLink :  https://www.youtube.com/watch?v=cHHU0jXfjKY&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=21
+// ytLink :  https://www.youtube.com/watch?v=eWwge2YpHhc&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=22
+
+// it is just baisc thing which i knew already so just go through video if want to 
+//  scope = { }
+if(true){
+    // .....
+}
